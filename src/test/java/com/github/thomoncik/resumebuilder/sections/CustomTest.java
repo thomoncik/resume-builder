1d4731f0f4dc20a6ee60e6bee17214de26a66732
Yuliia Buchko <yuliiabuchko@gmail.com>
2020-06-02 02:13:58 +0200

    Add References

---
+++ src/test/java/com/github/thomoncik/resumebuilder/sections/CustomTest.java
@@ -0,0 +1,13 @@
+package com.github.thomoncik.resumebuilder.sections;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomTest {
+
+
+    @Test
+    void process() {
+    }
+}
