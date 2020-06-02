4aeff1cdea0670078a3e960e686f068da2aca550
Yuliia Buchko <yuliiabuchko@gmail.com>
2020-06-03 00:37:43 +0200

    Add PersonalInformation

--- src/main/java/com/github/thomoncik/resumebuilder/sections/PersonalInformation.java
+++ src/main/java/com/github/thomoncik/resumebuilder/sections/PersonalInformation.java
@@ -34,10 +34,69 @@ public class PersonalInformation implements Section {
         public String dateOfBirth;
     }
 
+    private String resolveEmpty(String string) {
+        return string == null || string.isEmpty() || string.isBlank() ? "" : string;
+    }
+
+    private StringBuilder processImportantInformation(Details details){
+        StringBuilder result = new StringBuilder();
+        result.append("\\begin{INFO}{")
+                .append(resolveEmpty(details.jobTitle))
+                .append("}{")
+                .append(resolveEmpty(details.city))
+                .append("}{")
+                .append(resolveEmpty(details.phone))
+                .append("}\n")
+                .append("\\end{INFO}\n\n");
+        return result;
+    }
+
+    private StringBuilder processBirth(Details details){
+        StringBuilder result = new StringBuilder();
+        result.append("\\begin{BIRTH}{")
+                .append(resolveEmpty(details.dateOfBirth))
+                .append("}{")
+                .append(resolveEmpty(details.placeOfBirth))
+                .append("}\n")
+                .append("\\end{BIRTH}\n\n");
+        return result;
+    }
+
+    private StringBuilder processDriving(Details details){
+        StringBuilder result = new StringBuilder();
+        result.append("\\begin{DRIVING}{")
+                .append(resolveEmpty(details.drivingLicense))
+                .append("}\n")
+                .append("\\end{DRIVING}\n\n");
+        return result;
+    }
+
+    private StringBuilder processNationality(Details details){
+        StringBuilder result = new StringBuilder();
+        result.append("\\begin{NATIONALITY}{")
+                .append(resolveEmpty(details.nationality))
+                .append("}\n")
+                .append("\\end{NATIONALITY}\n\n");
+        return result;
+    }
+
+    private  StringBuilder processSummary(Summary summary){
+        StringBuilder result = new StringBuilder();
+        result.append("\\begin{SUMMARY}{")
+                .append(resolveEmpty(summary.description))
+                .append("}\n")
+                .append("\\end{SUMMARY}\n\n");
+        return result;
+    }
+
     @Override
     public StringBuilder process() {
-
-        // TODO generate
-        return null;
+        StringBuilder result = new StringBuilder();
+        result.append(processImportantInformation(this.details));
+        result.append(processBirth(this.details));
+        result.append(processNationality(this.details));
+        result.append(processDriving(this.details));
+        result.append(processSummary(this.summary));
+        return result;
     }
 }
