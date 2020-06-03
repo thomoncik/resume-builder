15fc00c5de9b185d24fc088a1486bfaaeaa5d88a
Tomasz Homoncik <tjhomoncik@gmail.com>
2020-06-03 23:25:44 +0200

    Sample database

---
+++ src/main/resources/db/migration/V2__create_user_table.sql
@@ -0,0 +1,10 @@
+create table resume_builder.user
+(
+    id            uuid         not null primary key,
+    first_name    varchar(100) not null,
+    last_name     varchar(100) not null,
+    email_name    varchar(100) not null,
+    password      varchar(256) not null,
+    enabled       boolean      not null,
+    token_expired boolean      not null
+);
